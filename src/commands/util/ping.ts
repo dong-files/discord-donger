05c81946ba8dc79fd692d555e34ffce1825edccb
Vielle.dev <git@vielle.dev>
2025-03-05 22:40:34 +0000

    add ping command

---
+++ src/commands/util/ping.ts
@@ -0,0 +1,16 @@
+import {
+  ChatInputCommandInteraction,
+  Client,
+  CommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
+import type { customClient } from "../..";
+
+export const data = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Replies with pong!");
+export const execute = async (
+  interaction: ChatInputCommandInteraction & { client: customClient }
+) => {
+  await interaction.reply("Pong!");
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,92 @@
-console.log("Hello via Bun!");
+import {
+  ChatInputCommandInteraction,
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  MessageFlags,
+  type Interaction,
+} from "discord.js";
+import { Glob } from "bun";
+
+const token = process.env.token;
+if (!token) throw new Error("Token required. Please fill in TOKEN in .env");
+console.log("Token Valid!");
+
+// client typing
+export type customClient = Client & {
+  // add command typing
+  commands: Collection<
+    string,
+    {
+      data: { name: string };
+      execute: (
+        interaction: ChatInputCommandInteraction & { client: customClient }
+      ) => Promise<void>;
+    }
+  >;
+};
+// new client
+const client: customClient = new Client({
+  intents: [GatewayIntentBits.Guilds],
+  // as customclient as i cannot add .commands till this is done
+}) as customClient;
+
+// setup commands
+client.commands = new Collection();
+const commandGlob = new Glob("**/*.ts");
+for await (const file of commandGlob.scan("./src/commands")) {
+  const command = await import("./commands/" + file);
+  // check command contains all required properties
+  if (
+    "data" in command &&
+    "execute" in command &&
+    typeof command.data === "object" &&
+    command.data !== null &&
+    "name" in command.data &&
+    typeof command.data.name === "string"
+  ) {
+    client.commands.set(command.data.name, command);
+    console.log("[loaded]", file);
+  } else {
+    // log missing features
+    console.log(`[WARNING] ${file} is not a valid command!`, command);
+  }
+}
+
+// when client is ready do this once
+client.once(Events.ClientReady, (ready) => {
+  console.log(`Ready! Logged in as ${ready.user.tag}`);
+});
+
+// _interaction so we can cast types properly
+// we need access to client.commands
+// we could just do it as a global but this makes it go wherever the command goes soooo
+client.on(Events.InteractionCreate, async (_interaction) => {
+  const interaction = _interaction as Interaction & { client: customClient };
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`No command ${interaction.commandName}`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (e) {
+    console.error(e);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
+});
+
+client.login(token);
